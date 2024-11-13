7eaf83fab9d135c18171edcc059c1475abdfdd49
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-11-13 19:59:57 +0630

    feat(api): test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Post } from '@nestjs/common';
 import { IResponse } from './core/interfaces/response.interface';
 
 @Controller()
@@ -9,6 +9,28 @@ export class AppController {
       _data: new Date(),
       _metadata: {
         message: 'Health check success.',
+        statusCode: HttpStatus.OK,
+      },
+    };
+  }
+
+  @Get('/test1')
+  test1(): IResponse {
+    return {
+      _data: { test: 'one' },
+      _metadata: {
+        message: 'test one success.',
+        statusCode: HttpStatus.OK,
+      },
+    };
+  }
+
+  @Post('/test2')
+  test2(): IResponse {
+    return {
+      _data: { test: 'two' },
+      _metadata: {
+        message: 'test two success.',
         statusCode: HttpStatus.OK,
       },
     };
